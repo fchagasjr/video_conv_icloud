36e355feb14f3ae07a7b1f1aba3923abb9e17c9f
Francisco das Chagas S. Jr <79976550+fchagassjr@users.noreply.github.com>
2022-04-02 15:02:53 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -18,7 +18,7 @@ def collect_files(file_types):
     return file_zip
 
 
-def convert_files(file_zip):
+def convert_files(file_zip, api):
     for file_name, file_path in file_zip:
         if output_path not in file_path:
             print(f'Started processing: {file_name}\n{file_path}')
@@ -59,51 +59,55 @@ def create_dir(file_path, file_name):
     return new_dir
 
 
-user_name = input('User name: ')
-password = input('Password: ')
-api = PyiCloudService(user_name, password)
+def connect_to_cloud():
+    user_name = input('User name: ')
+    password = input('Password: ')
+    api = PyiCloudService(user_name, password)
 
-if api.requires_2fa:
-    print("Two-factor authentication required.")
-    code = input("Enter the code you received of one of your approved devices: ")
-    result = api.validate_2fa_code(code)
-    print("Code validation result: %s" % result)
-
-    if not result:
-        print("Failed to verify security code")
-        sys.exit(1)
-
-    if not api.is_trusted_session:
-        print("Session is not trusted. Requesting trust...")
-        result = api.trust_session()
-        print("Session trust result %s" % result)
+    if api.requires_2fa:
+        print("Two-factor authentication required.")
+        code = input("Enter the code you received of one of your approved devices: ")
+        result = api.validate_2fa_code(code)
+        print("Code validation result: %s" % result)
 
         if not result:
-            print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
-elif api.requires_2sa:
-    import click
-    print("Two-step authentication required. Your trusted devices are:")
+            print("Failed to verify security code")
+            sys.exit(1)
 
-    devices = api.trusted_devices
-    for i, device in enumerate(devices):
-        print("  %s: %s" % (i, device.get('deviceName',
-            "SMS to %s" % device.get('phoneNumber'))))
+        if not api.is_trusted_session:
+            print("Session is not trusted. Requesting trust...")
+            result = api.trust_session()
+            print("Session trust result %s" % result)
 
-    device = click.prompt('Which device would you like to use?', default=0)
-    device = devices[device]
-    if not api.send_verification_code(device):
-        print("Failed to send verification code")
-        sys.exit(1)
+            if not result:
+                print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
+    elif api.requires_2sa:
+        import click
+        print("Two-step authentication required. Your trusted devices are:")
 
-    code = click.prompt('Please enter validation code')
-    if not api.validate_verification_code(device, code):
-        print("Failed to verify verification code")
-        sys.exit(1)
+        devices = api.trusted_devices
+        for i, device in enumerate(devices):
+            print("  %s: %s" % (i, device.get('deviceName',
+                "SMS to %s" % device.get('phoneNumber'))))
+
+        device = click.prompt('Which device would you like to use?', default=0)
+        device = devices[device]
+        if not api.send_verification_code(device):
+            print("Failed to send verification code")
+            sys.exit(1)
+
+        code = click.prompt('Please enter validation code')
+        if not api.validate_verification_code(device, code):
+            print("Failed to verify verification code")
+            sys.exit(1)
+    
+    return api            
 
 
 if __name__ == '__main__':
+    user_api=connect_to_cloud()
     file_types = [
          '3gp', '3GP', 'MKV', 'mkv', 'WMV', 'wmv','MP4', 'mp4', 'AVI', 'avi']
     files_to_convert = collect_files(file_types)
-    convert_files(files_to_convert)
+    convert_files(files_to_convert, user_api)
     
